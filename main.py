bb1a0e4ba443b2b49b1f9d61ac3ebc4f957c6928
lkokot <lkokot@student.ethz.ch>
2021-06-17 23:57:14 +0200

    Implemented initial working version of algorithm

--- main.py
+++ main.py
@@ -3,53 +3,91 @@ from models import DenseNet
 import torch
 from torch import nn
 import numpy as np
+from train import train_network
 
 from util import DirectedGraph
 
-LAYERS_NEURONS=[128,64,16]
-LEARNING_RATE=0.01
+LAYERS_NEURONS=[128,64,16,8]
+LEARNING_RATE=0.0001
 
 N_ITER=200000#NUMBER OF ITERATIONS/GENERATIONS
-N_SESSIONS_PER_ITER=200
-TRAIN_PERCENTILE=7#top percentile we are learning from each iteration
-SUPER_PERCENTILE=6#top percentile of sesstions that live to the next generation
+N_SESSIONS_PER_ITER=5000
+TRAIN_PERCENTILE=10#top percentile we are learning from each iteration
+SUPER_PERCENTILE=1#top percentile of sesstions that live to the next generation
 
-def play_game(model, graph, cur_state, action_prob)
+
+def make_choice(model, graph : DirectedGraph, cur_state):
+    action_prob : torch.tensor = nn.Softmax(dim=1)(model(cur_state[None, :]))[0,:]  # make input be 2D before giving it to model and then reverse
+
+    valid_actions=util.extract_legal_actions(cur_state)
+    visited_nodes=util.extract_visited_nodes(cur_state)
+
+    action=np.random.choice([i for i in range(graph.N)], p=action_prob.detach().numpy())
+    if action not in valid_actions:
+        return action, 0, True
+    if action in visited_nodes:
+        return action, 0, True
+
+    return action, 1, False
 
 def initilize_model_state(graph : DirectedGraph):
-    init_state=[0]*(graph.num_vertices*3)
+    init_state=[0]*(graph.N*3)
 
-    #we start from node 0
-    init_state[0]=1
+    #set 1 to index of current node (we start from node 0)
+    offset=0
+    init_state[offset+28]=1
+
 
     #set values to 1 at indices of nodes that are reachable from current node
-    reachable_nodes=graph.edge_list[0]
-    for i in range(graph.edge_cnt[0]):
+    offset=graph.N
+    reachable_nodes=graph.edge_list[28]
+    for i in range(graph.edge_cnt[28]):
         reachable_node=reachable_nodes[i]
-        init_state[reachable_node]=1
+        init_state[offset+reachable_node]=1
 
     #set values of visited nodes so far to 1
-    init_state[0]=1#not needed but for explanatory purposes this code is here
+    offset=graph.N*2
+    init_state[offset+28]=1#not needed but for explanatory purposes this code is here
 
-    return np.array(init_state)
+    return torch.from_numpy(np.array(init_state)).float()
+
+def update_model_state(cur_state, action, graph : DirectedGraph):
+    next_state=[0]*(graph.N*3)
+
+    #set 1 to index of current node
+    offset=0
+    next_state[offset+action]=1
+
+    #set 1 to indices of nodes that are reachable from current node
+    offset=graph.N
+    reachable_nodes=graph.edge_list[action]
+    for i in range(graph.edge_cnt[action]):
+        reachable_node=reachable_nodes[i]
+        next_state[offset+reachable_node]=1
+
+
+    #set values of visited nodes so far to 1
+    offset=graph.N*2
+    next_state[offset:]=cur_state[offset:]
+    next_state[offset+action]=1
+
+    return torch.from_numpy(np.array(next_state)).float()
 
 
 def generate_session(model, graph):
     state_action_seq,reward=[],0
-    softmax_fn=nn.Softmax(dim=0)
 
     cur_state=initilize_model_state(graph)
     while True:
-        action_prob=softmax_fn(model(cur_state))
+        action, transition_reward, terminal=make_choice(model, graph, cur_state)
+        reward+=transition_reward
 
-        action, transition_reward, next_state, terminal=play_game(model, graph, cur_state, action_prob)
         state_action_seq.append((cur_state, action))
 
         if terminal:
-            reward=transition_reward
             break
 
-        cur_state=next_state
+        cur_state=update_model_state(cur_state, action, graph)
 
     return (state_action_seq, reward)
 
@@ -58,33 +96,87 @@ def generate_sessions(model, graph, n_sessions):
     Session consists of list that looks like: {[(s1,a1),(s2,a2),...,(sn,an)],r}, si=state in step i, ai=action in step i
     Code generates list of such sessions
     """
-    sessions=np.empty((n_sessions, ), dtype=object)
-    sessions.fill([])
+    sessions=[None]*n_sessions
     for i in range(n_sessions):
-        sessions[i].append(generate_session(model))
+        sessions[i]=generate_session(model, graph)
 
     return sessions
 
-def obtain_graph_walker_policy(graph):
-    N=graph.num_vertices()
+def select_top_sessions(sessions, percentile):
+    N=len(sessions)
+    top_num=int(1+N*percentile/100)
 
+    sessions.sort(key=lambda x : x[1], reverse=True)
+    return sessions[:top_num]
+
+def select_super_sessions(sessions):
+    return select_top_sessions(sessions, SUPER_PERCENTILE)
+
+
+def select_elite_sessions(sessions):
+    return select_top_sessions(sessions, TRAIN_PERCENTILE)
+
+
+def obtain_graph_walker_policy(graph):
     #input to agent (neural network) consists of 3 parts each of length N
     #first part has 1 on index of next vertex we plan to move to else 0
     #second part has 1 on index of current vertex else 0
     #third part has 1 on index of (so far) visited vertex else 0
-    word_length=3*N
+    word_length=3*graph.N
     #output of agent as probability distribution over which state to choose next to go to
     #need to be careful because it might be that agent tries to go to next state that is not
     #offered to go to from current state
-    action_space_length=N
+    action_space_length=graph.N
 
     model = DenseNet([word_length, *LAYERS_NEURONS, action_space_length])
     optimizer=torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+    super_sessions = None
+
+    mean_reward_vals=[]
     for i in range(N_ITER):
-        sessions = generate_sessions(model, graph, N_SESSIONS_PER_ITER)
+        sessions : list(tuple(list(tuple), int)) = generate_sessions(model, graph, N_SESSIONS_PER_ITER)
+
+        if i>0:
+            #append super sessions (top SUPER_PERCENTILE from previous iteration) to current sessions
+            sessions+=super_sessions
+
+        elite_sessions=select_elite_sessions(sessions)#select sessions on which we train the agent
+        super_sessions=select_super_sessions(sessions)#select sessions which survive to next iteration
+
+        train_x, train_y=np.empty((0,3*graph.N)), np.empty((0,1))
+        for state_action_pairs,_ in elite_sessions:
+            for state, action in state_action_pairs:
+                train_x=np.append(train_x, state.detach().numpy()[None,:], axis=0)
+                train_y=np.append(train_y, np.array([[action]]), axis=0)
+
+        train_dataset=torch.from_numpy(np.column_stack((train_x, train_y))).float()
+        train_loader=torch.utils.data.DataLoader(train_dataset, shuffle=True, batch_size=16)
+
+        train_network(model, torch.nn.CrossEntropyLoss(), optimizer, train_loader)
+
+        reward_super_sessions_mean=np.mean([el[1] for el in super_sessions])
+        print('Mean reward of top {} % sessions = {}'.format(SUPER_PERCENTILE, reward_super_sessions_mean))
+
+        if i>20 and sum([abs(reward_super_sessions_mean-mean_reward_vals[j]) for j in range(-1,-15,-1)])<0.1:
+            best_session_state_action_pairs,best_session_reward=super_sessions[0]
+            print('Best session reward: {}'.format(best_session_reward))
+            print(28,end='')
+            for state, action in best_session_state_action_pairs:
+                print('->{}'.format(action),end='')
+
+            print()
+            break
+
+        mean_reward_vals.append(reward_super_sessions_mean)
 
 
 if __name__=='__main__':
-    obtain_graph_walker_policy(util.build_graph())
+    obtain_graph_walker_policy(util.build_graph2())
+
+# [28 20 49 34 32  2 35 24  0 48 45 23 13  6  5 37 31 25  4 14 19 17 30 26
+#  47 39 12 29 27  9 11 44 15 42 21 41 43 40 36 33  7 38  1  3  8 16 10 46
+#  18 22]
+#
+# 28->24->43->9->20->0->40->34->8->4->23->36->48->30->33->13->25->2->15->32->11->49->10->45->37->47->38->26->14->16->35->42->18
 
--- util.py
+++ util.py
@@ -4,16 +4,31 @@ class DirectedGraph:
 
     def __init__(self, N):
         self.N=N
-        self.adj_matr=np.zeros((N,N))
-        self.edge_list=np.full((N,N), None)
-        self.edge_cnt=np.zeros(N)
+        self.adj_matr=np.zeros((N, N), dtype=np.int8)
+        self.edge_list=np.full((N, N), None)
+        self.edge_cnt=np.zeros(N, dtype=np.int16)
 
     def add_edge(self, v1,v2):
+        if self.adj_matr[v1][v2]==1:
+            return
+
         self.adj_matr[v1][v2]=1
         self.edge_list[v1][self.edge_cnt[v1]]=v2
         self.edge_cnt[v1]+=1
 
+    def __repr__(self):
+        str='N: {}'.format(self.N)
+        str+=', edges: '
+        for v1 in range(self.N):
+            for v2 in self.edge_list[v1]:
+                if v2 is None:
+                    break
 
+                str+='{}->{}, '.format(v1,v2)
+
+        str=str[:-2]
+
+        return str
 
 def build_graph():
     graph=DirectedGraph(5)
@@ -22,3 +37,37 @@ def build_graph():
         graph.add_edge(*edge)
 
     return graph
+
+def build_graph2():
+    np.random.seed(73)
+
+    N=50
+    rand_perm=np.random.permutation(N)
+    print(rand_perm)
+
+    graph=DirectedGraph(N)
+    for i in range(N-1):
+        graph.add_edge(rand_perm[i], rand_perm[i+1])
+
+    for i in range(N*(N-1)//5):
+        graph.add_edge(np.random.randint(0, N), np.random.randint(0, N))
+
+    return graph
+
+def extract_legal_actions(state : list):
+    N=len(state)//3
+
+    filter=state[N:2*N]
+    return set(np.where(filter==1)[0])
+
+
+def extract_visited_nodes(state : list):
+    N=len(state)//3
+
+    filter=np.array(state[2*N:])
+    return set(np.where(filter==1)[0])
+
+if __name__=='__main__':
+    graph=build_graph2()
+    print(graph)
+
